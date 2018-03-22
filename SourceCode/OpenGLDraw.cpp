5d821167650ba57bb897c5fafaf9d4fbf6d27c1d
ShuChenATJp <shuchenjp@gmail.com>
2018-03-22 13:11:45 +0900

    Decoration
    Decoration

--- SourceCode/OpenGLDraw.cpp
+++ SourceCode/OpenGLDraw.cpp
@@ -83,7 +83,7 @@ void Init()
   SystemTime.Writelast_time(glutGet(GLUT_ELAPSED_TIME));
   SystemTime.Writeframe_count(0);
   SystemTime.Writeframe_time(0);
-  cams[0] = Camera(Point(0.0f, 0.0f, 0.0f), Vector(0.0f, 0.0, -1.0f), RelativeUp
+  cams[0] = Camera(Point(0.0f, 0.0f, 50.0f), Vector(0.0f, 0.0, -1.0f), RelativeUp
                 , 0.5f*PI, 1, 0.01f, 1.0f);          
   SSStexture = &SSS_Texture();
   Object* tmp = new Object(Index, SSStexture->GetTexture(0));//TMP
@@ -91,10 +91,40 @@ void Init()
 	tmp->DefaultScale = Vector(1, 1, 1);
 	tmp->DefaultRotation = Point(-1 * 3.1415926/2, 0, 0);
   tmp->WriteTranslation(0, 0, 0);
-	tmp->WriteDefaultPosition(0, 0, -5);
+	tmp->WriteDefaultPosition(0, 0, -13);
 	tmp->WriteRotation(0, 0.01, 0);
 	tmp->WriteSelfRotation(0.00, 0.03, 0.0);
   AddObject(tmp);
+	
+	Object* tmp1 = new Object(Index, SSStexture->GetTexture(0));//TMP
+	tmp1->WriteScale(1, 1, 1);
+	tmp1->DefaultScale = Vector(7, 7, 7);
+	tmp1->DefaultRotation = Point(-1 * 3.1415926/2, 0, 0);
+  tmp1->WriteTranslation(0, 0, 0);
+	tmp1->WriteDefaultPosition(0, 0, 0);
+	tmp1->WriteRotation(0, 0.0, 0);
+	tmp1->WriteSelfRotation(0.00, 0.0, 0.0);
+	AddObject(tmp1);
+	
+	Object* tmp2 = new Object(Index, SSStexture->GetTexture(0));//TMP
+	tmp2->WriteScale(1, 1, 1);
+	tmp2->DefaultScale = Vector(2.3, 2.3, 2.3);
+	tmp2->DefaultRotation = Point(-1 * 3.1415926/2, 0, 0);
+  tmp2->WriteTranslation(0, 0, 0);
+	tmp2->WriteDefaultPosition(0, 0, -20);
+	tmp2->WriteRotation(0, 0.002, 0);
+	tmp2->WriteSelfRotation(0.00, 0.01, 0.0);
+	AddObject(tmp2);
+	
+	Object* tmp3 = new Object(Index, SSStexture->GetTexture(0));//TMP
+	tmp3->WriteScale(1, 1, 1);
+	tmp3->DefaultScale = Vector(4, 4, 4);
+	tmp3->DefaultRotation = Point(-1 * 3.1415926/2, 0, 0);
+  tmp3->WriteTranslation(0, 0, 0);
+	tmp3->WriteDefaultPosition(0, 0, -27);
+	tmp3->WriteRotation(0, 0.001, 0);
+	tmp3->WriteSelfRotation(0.00, 0.002, 0.0);
+	AddObject(tmp3);
 }
 
 void Draw()
@@ -136,7 +166,7 @@ void Draw()
   for (std::vector<Object*>::size_type i = 0; i < Index; i++)
   {
     ObjectList[i]->ObjectDraw(tmpCameraAffine, tmpProjecMatrix, obj_initialized);
-    ObjectList[i]->WriteAngle(ObjectList[i]->GetAngle() + 1);
+    //ObjectList[i]->WriteAngle(ObjectList[i]->GetAngle() + 1);
   }
   // swap in the back buffer
   glutSwapBuffers();
